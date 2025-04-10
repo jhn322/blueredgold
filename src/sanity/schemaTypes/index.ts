ff9b0e443107c063902b881ef4693131063685a6
JerRoyDev <jerryroyolsson@gmail.com>
2025-04-10 14:28:53 +0200

    Uppdatera struktur och schema för innehåll: Ändrade titeln från 'Blog' till 'Content' och uppdaterade dokumenttyper i strukturen för att inkludera 'Recipes' och 'Articles'. Lagt till ny 'articleType' och 'recipeType' med nödvändiga fält och valideringar. Tog bort den gamla 'postType' för att förenkla strukturen. Justerade blockinnehållstypen för att inkludera numrerade listor.
    ---------
    Co-authored-by: JerRoyDev <jerryroyolsson@gmail.com>
    Co-authored-by: jhn322 <johan.soderlund96@gmail.com>

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,10 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { blockContentType } from './blockContentType'
+import { categoryType } from './categoryType'
+import { authorType } from './authorType'
+import { recipeType } from './recipeType'
+import { articleType } from './articleType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [blockContentType, categoryType, authorType, recipeType, articleType],
 }
